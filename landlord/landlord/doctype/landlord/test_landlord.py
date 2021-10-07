f35386004d6afa14198b126e601f6ec8d7cbc697
Ubuntu <ubuntu@ip-172-31-25-109.us-west-2.compute.internal>
2021-10-07 16:19:30 +0000

    first commit

---
+++ landlord/landlord/doctype/landlord/test_landlord.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, JMG and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestLandlord(unittest.TestCase):
+	pass
---
+++ landlord/landlord/doctype/property/test_property.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, JMG and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestProperty(unittest.TestCase):
+	pass
